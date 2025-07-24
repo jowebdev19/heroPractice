e5a88eb56bbe8802e179759aa420a87a581bcd23
Josiah <jowebdev5021@gmail.com>
2025-07-24 14:13:07 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <div>
+      <nav>
+        <h1>Village Eats</h1>
+        <ul>
+          <li>Home</li>
+          <li>Recipes</li>
+          <li>Favorites</li>
+        </ul>
+      </nav>
+      <div className='hero-container'>
+        <img src='/vegetables-9132663_1280.jpg' />
+        <p>Tons of delicious recipes all in one place</p>
+        <button>View Recipes</button>
+      </div>
+     </div>
+    </>
+  )
+}
+
+export default App
